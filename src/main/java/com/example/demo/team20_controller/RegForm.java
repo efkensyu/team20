5d649f1523bfbc72c0c9df6c2fb31bccc94354b7
choju <choju>
2026-05-21 11:30:26 +0900

    C21.2 register修正

--- src/main/java/com/example/demo/team20_controller/RegForm.java
+++ src/main/java/com/example/demo/team20_controller/RegForm.java
@@ -1,28 +1,28 @@
 package com.example.demo.team20_controller;
 
-import jakarta.validation.constraints.NotBlank;
-
 import lombok.Data;
 
 @Data
 public class RegForm {
 	
-	@NotBlank
+	
 	private String name;
-	@NotBlank
+	
+	private String code;
+	
 	private String janru;
-	@NotBlank(message = "いずれかを選択してください")
+	
 	private String hobby;
-	@NotBlank
+	
 	private String janru2;
-	@NotBlank(message = "いずれかを選択してください")
+	
 	private String hobby2;
-	@NotBlank
+	
 	private String janru3;
-	@NotBlank(message = "いずれかを選択してください")
+	
 	private String hobby3;
 	
-	private String info;
+	private String job;
 	
 	private String intro;
 	
