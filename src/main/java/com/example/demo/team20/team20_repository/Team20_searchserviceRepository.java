02cbe9bc37bc3d50bcc543dc23ba717d218d9039
s.kurihara <sh_kurihara@bold.ne.jp>
2026-05-26 17:45:02 +0900

    k26.7 Searchファイルlogic1chousei

--- src/main/java/com/example/demo/team20/team20_repository/Team20_searchserviceRepository.java
+++ src/main/java/com/example/demo/team20/team20_repository/Team20_searchserviceRepository.java
@@ -11,7 +11,8 @@ import com.example.demo.team20.team20_entity.Team20_Shain;
 @Repository
 public interface Team20_searchserviceRepository extends JpaRepository<Team20_Shain,String>{
 	@Query(value="select * from team20_shain_tbl s "+
-			" where (coalesce(:name, '') = '' or s.shainnm like '%'||:name||'%') " +
+			"where (coalesce(:loginUserid, '') = '' or s.shaincd != :loginUserid) " +
+			"and (coalesce(:name, '') = '' or s.shainnm like concat('%',:name,'%')) " +
 			" and (coalesce(:job, '') = '' or lower(s.job )= lower(:job)) " +
             " and ( " +
             "   (coalesce(:janru, '') = '' and coalesce(:hobby, '') = '') " +
@@ -20,5 +21,5 @@ public interface Team20_searchserviceRepository extends JpaRepository<Team20_Sha
             "   or s.rank3 in (select h.hobbycode from team20_hobby_tbl h where (coalesce(:janru, '') = '' or h.janru = :janru) and (coalesce(:hobby, '') = '' or h.hobby = :hobby))) "
 				,nativeQuery=true)
 	public List<Team20_Shain> findmatch(@Param("name") String name,@Param("janru")String janru,@Param("hobby")
-	String hobby,@Param("job")String job);
+	String hobby,@Param("job")String job,@Param("loginUserid") String loginUserid);
 }
